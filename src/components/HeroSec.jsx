8fc76b26362b6680e1876481be7cf7373035db92
ratneshchipre <ratneshchipre@gmail.com>
2025-04-18 17:20:54 +0530

    Deployment error, changed name of img file

--- src/components/HeroSec.jsx
+++ src/components/HeroSec.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import uploadPitchImg from "../assets/uploadPitchImg.png";
-import gridTunnelimg from "../assets/gridTunnelimg.jpg";
+import gridTunnelImg from "../assets/gridTunnelImg.jpg";
 
 const HeroSec = ({ sectionRef }) => {
     const scrollToSection = () => {
@@ -20,7 +20,7 @@ const HeroSec = ({ sectionRef }) => {
     return (
         <div className='flex flex-col justify-center items-center w-full px-[2rem] mini-desktop:px-[3rem] lg:px-[4rem] xl:px-[8rem] pt-[8rem] pb-[3rem] overflow-hidden'>
             <div className='w-full fade-bottom-mask absolute z-0 mt-[-3rem] overflow-hidden'>
-                <img src={gridTunnelimg} alt="" className='w-full h-full object-fill opacity-15 mix-blend-screen' />
+                <img src={gridTunnelImg} alt="" className='w-full h-full object-fill opacity-15 mix-blend-screen' />
             </div>
             <div className='flex flex-col items-center z-10 gap-[1rem] xl:gap-[1.5rem] text-center'>
                 <h1 className='text-[2.5rem] tablet:text-[2.7rem] xl:text-[3.2rem] text-txt-black text-center font-Bold leading-[3.5rem] xl:leading-[4rem] mini-desktop:w-[60%] mini-desktop:text-[3rem]'>Where <span className='bg-gradient-to-r from-[#1E40AF] to-[#0EA5E9] bg-clip-text text-transparent'>Bold Ideas</span> Meet the Right Eyes.</h1>
